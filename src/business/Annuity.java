d7f9a54ccc6acb9d1b7c75ddd531c338e6c753f4
Jasmine Herd <jthm66@mail.missouri.edu>
2018-07-22 01:31:58 -0500

    finished

--- src/business/Annuity.java
+++ src/business/Annuity.java
@@ -6,80 +6,67 @@ package business;
  * @author jasmineherd
  */
 public class Annuity {
-    //Class Constants-static value
-    public static final String AMTDESC= "Monthly Deposit:";//amount description/
-                                                         //final-FINAL!
-    public static final String  RESULTDESC = "Final Annuity Value:";
+    public static final String AMTDESC = "Monlthy Deposit: ";
+    public static final String RESULTDESC = "Final Annuity Value: ";
     
     private double amt,rate;
     private int term;
     private String errmsg;
     private double[] begbal,intearn,endbal;
     private boolean built;
-    
-    
-    
-    // constructor-runs when new keyword is used
-    //save these params into global variables for the class
-    //forms cannot directly access these private values.They belong to Class
-    public Annuity(double a,double r, int t){
-        this.amt=a;
+    public Annuity(double a, double r, int t) {
+        this.amt = a;
         this.rate = r;
-        this.term = t;
-        this.built=false;
-        //more validation
+        this.term = t;  
+        built = false;
         if(isValid()){
             buildAnnuity();
-        }
+        }  
+      
     }
+ 
     
     private boolean isValid(){
         boolean valuesok = true;
-        //this refers to globals in class.This one not That one....
-        this.errmsg="";
-        if(this.amt<=0){
-            this.errmsg= "Bad amount:not a positive value";
-            valuesok=false;
+        this.errmsg = "";
+        if(this.amt <=0){
+            this.errmsg = "Bad amount: not a positive value";
+            valuesok = false;
         }
-        if(this.rate<=0){
-            this.errmsg+="Bad Rate: not positive";
-            valuesok=false;
+        if(this.rate <= 0 ){
+            this.errmsg += " Bad Rate: not positive";
+            valuesok = false;
         }
-        if(this.term<=0){
-            this.errmsg+="Bad Term: not positive";
-            valuesok= false;
+        if(this.term <= 0){
+            this.errmsg += " Bad Term: not positive";
+            valuesok = false;
         }
         return valuesok;
-    }//isValid
-    
-    
+    }
     
     private void buildAnnuity(){
-        
-        this.errmsg="";
-                
+        this.errmsg = "";
         try{
-            this.begbal = new double[this.term];
-            this.intearn = new double[this.term];
-            this.endbal = new double[this.term];
+            this.begbal  = new double[this.term];
+            this.intearn  = new double[this.term];
+            this.endbal  = new double[this.term];
             
-            this.begbal[0]=0.0;
-            for(int i =0;i<this.term;i++){
-                
+            this.begbal[0] = 0.0;
+            for(int i =0; i< this.term;i++){
                 if(i>0){
-                    this.begbal[i] = this.endbal[i-1];
+                    this.begbal[i]=this.endbal[i-1];
                 }
-                this.intearn[i] = (this.begbal[i]+this.amt)*(this.rate/12);
-                this.endbal[i] = begbal[i]+this.intearn[i]+this.amt;
-                
+                this.intearn[i]= (this.begbal[i] + this.amt) * (this.rate/12.0);
+                this.endbal[i] = this.begbal[i] + this.intearn[i]+ this.amt; 
             }
-            this.built= true;
+            this.built = true;
         }catch(Exception e){
-            this.errmsg = "build error: "+e.getMessage();
-            this.built=false;
+                    this.errmsg = "build error: " + e.getMessage();
+                    this.built = false;
         }
-        
+    
     }
+    
     public String getErrorMsg(){
         return this.errmsg;
     }
@@ -106,35 +93,34 @@ public class Annuity {
             }
         }
         if(mo < 1 || mo > this.term){
-            //month is out of range
             return -1;
         }
-        return this.begbal[mo-1];//remember *fences* array-1 for last index
+        return this.begbal[mo-1];
     }
-    public double getIntEarned(int mo){
-         if(!built){
+    public double getIntEarn(int mo){
+        if(!built){
             buildAnnuity();
             if(!built){
                 return -1;
             }
         }
         if(mo < 1 || mo > this.term){
-            //month is out of range
             return -1;
         }
         return this.intearn[mo-1];
     }
     public double getEndBal(int mo){
-         if(!built){
+        if(!built){
             buildAnnuity();
             if(!built){
                 return -1;
             }
         }
         if(mo < 1 || mo > this.term){
-            //month is out of range
             return -1;
         }
         return this.endbal[mo-1];
     }
+    
 }
+
--- src/business/Loan.java
+++ src/business/Loan.java
@@ -6,48 +6,27 @@ package business;
  * @author jasmineherd
  */
 public class Loan {
-     public static final String AMTDESC= "Loan Amount:";
-    public static final String  RESULTDESC = "Monthly Payment:";
+    public static final String AMTDESC = "Loan Amount: ";
+    public static final String RESULTDESC = "Monthly Deposit" ;
     
-    private double amt,rate,mopmt;//mopmt=monthlh payment 
-    private int term;//term = months
+    private double amt,rate,mopmt;  
+    private int term;
     private String errmsg;
     private boolean built;
     private double[] begbal,intchg,endbal;
     
-    
-public Loan(double a,double r,int t){
-this.amt = a;
-this.rate = r;
-this.term = t;//term=months
-this.built = false;
-this.mopmt = 0;
-this.errmsg= "";
-if(isValid()){
-    buildLoan();
-}
+    public Loan(double a, double r, int t){
+        this.amt = a;
+        this.rate = r;
+        this.term = t;
+        this.built= false;
+        this.mopmt = 0.0;
+        this.errmsg = ""; 
+        if(isValid()){
+            buildLoan();
+        }
         
-}
-
-    private boolean isValid(){
-        boolean valuesok = true;
-        //this refers to globals in class.This one not That one....
-        this.errmsg="";
-        if(this.amt<=0){
-            this.errmsg= "Bad amount:not a positive value";
-            valuesok=false;
-        }
-        if(this.rate<=0){
-            this.errmsg+="Bad Rate: not positive";
-            valuesok=false;
-        }
-        if(this.term<=0){
-            this.errmsg+="Bad Term: not positive";
-            valuesok= false;
-        }
-        return valuesok;
-    }//isValid
-    
+    }
     public double getAmt(){
         return this.amt;
     }
@@ -60,23 +39,34 @@ if(isValid()){
     public String getErrorMsg(){
         return this.errmsg;
     }
-    
     private void buildLoan(){
         this.errmsg = "";
         try{
-            double morate = this.rate / 12.0; //get monthly rate
-            double denominator = Math.pow((1+morate),this.term)-1;
-            this.mopmt =(morate + morate/ denominator) * this.amt;
-            //calc array values
+            double morate = this.rate/12.0;
+            double denom = Math.pow((1+morate), this.term)-1;
+            this.mopmt = (morate + morate/denom)* this.amt;
+            
+            this.begbal = new double[this.term];
+            this.intchg = new double [this.term];
+            this.endbal = new double[this.term];
+            
+            this.begbal[0] = this.amt;
+            for(int i =0;i<this.term;i++){
+                if(i>0){
+                    this.begbal[i] = this.begbal[i-1];
+                }
+                this.intchg[i]=this.begbal[i]*morate;
+                this.endbal[i]=(this.begbal[i]+this.intchg[i])-this.mopmt;
+            }
             
             
-            this.built = true;
-        }catch(Exception e ){
+            this.built =true;
+        }catch(Exception e){
             this.errmsg = "Build error: "+ e.getMessage();
             this.built = false;
-                    
+            
         }
-    }//end of buildLoan
+    }
     public double getResult(){
         if(!built){
             buildLoan();
@@ -86,9 +76,25 @@ if(isValid()){
         }
         return this.mopmt;
     }
+    private boolean isValid(){
+        boolean valuesok = true;
+        this.errmsg = "";
+        if(this.amt <=0){
+            this.errmsg = "Bad amount: not a positive value";
+            valuesok = false;
+        }
+        if(this.rate <= 0 ){
+            this.errmsg += " Bad Rate: not positive";
+            valuesok = false;
+        }
+        if(this.term <= 0){
+            this.errmsg += " Bad Term: not positive";
+            valuesok = false;
+        }
+        return valuesok;
+    }
     
 }
-
 
 
 
